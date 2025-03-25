bd2d1b6ab613e4a731956265a3051ed978014d5d
dhu2023 <dan.hu@onepeloton.com>
2025-03-25 13:54:24 -0400

    remove default args

--- dagfactory/dagfactory.py
+++ dagfactory/dagfactory.py
@@ -132,7 +132,7 @@ class DagFactory:
                         if owner == 'data_engineering':
                             default_config['tags'] = sub_fpath.split("/")[5:7]
                             if sub_fpath.split("/")[5] in ('etl', 'reverse_etl') and not any(
-                        tag.startswith("criticality:tier") for tag in default_config['default_args']["tags"]):
+                        tag.startswith("criticality:tier") for tag in default_config["tags"]):
                                 default_config["tags"].append("criticality:tier2")
                     else:
                         logger.info(f"Ignored invalid dag config file: {sub_fpath} ")
